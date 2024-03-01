615db171cc0fc8e29e663a64a29bc8aa24558a7a
Nil19 <nilufar_adat@hotmail.com>
2024-03-01 15:44:16 +0000

    updated pomodoro timer

--- index.js
+++ index.js
@@ -1,135 +1,69 @@
-// Interaction 1
-// when I click the play button
-// - change the icon to a pause button
-// - decrease counter
-// - animations start
+// variables
 
-// Interaction 2
-// when first timer ends, second timer starts
-// - change the background colour
-// - decrease counter
+let workTittle = document.getElementById('work');
+let breakTittle = document.getElementById('break');
 
-// Interaction 3
-// When break time ends
-// - stop everything
+let workTime = 25;
+let breakTime = 5;
 
-// Interaction 4
-// When I click the add 1 minute button
-// - add time to the current timer
+let seconds = "00"
 
-// Interaction 5
-// As timer decrements
-// - rotate the arm
-// - change the length of the svg circle
+// display
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
 
-// Interaction 6
-// When I click the pause button
-// - animations stop
-// - timers stop
-// - button changes from pause to play
-
-// declare variables that hold unchanging state
-const controlBtn = document.querySelector(".control-btn")
-const workTimer = document.querySelector("#workTimer")
-const breakTimer = document.querySelector("#breakTimer")
-const addTimeBtn = document.querySelector("#addTimeBtn")
-const arm = document.querySelector(".arm")
-const circle = document.querySelector("#circleTimer")
-const workSeconds = 10
-const breakSeconds = 10
-const increment = 60
-const circumference = 2 * Math.PI * 50
-
-// declare variables that hold changing state
-let isPaused, timerInterval, currentSeconds, currentTimer, isWorkPhase, currentTotal
-isPaused = true
-isWorkPhase = true
-currentSeconds = workSeconds
-currentTotal = workSeconds
-currentTimer = workTimer
-circle.style.strokeDasharray = circumference
-circle.style.strokeDashoffset = circumference
-
-// Initialise app
-updateTimer(workTimer, workSeconds)
-updateTimer(breakTimer, breakSeconds)
-
-controlBtn.addEventListener("click", () => {
-  // toggle isPaused value
-  isPaused = !isPaused
-  // apply the appropriate control icon to the element
-  controlBtn.classList.toggle("control-btn--pause", !isPaused)
-  // start the timer
-  if (!timerInterval) {
-    timerInterval = setInterval(decrement, 1000)
-  }
-})
-
-addTimeBtn.addEventListener("click", () => {
-  // add to the total time
-  currentSeconds += increment
-  currentTotal += increment
-
-  //update the timer
-  updateTimer(currentTimer, currentSeconds)
-})
-
-function updateTimer(timer, numSeconds) {
-  const minutes = Math.floor(numSeconds / 60) //integer divisions
-  const seconds = numSeconds % 60
-  const formattedSeconds = seconds.toString().padStart(2, '0')
-  // calculate the right time before updating
-  timer.innerText = `${minutes}:${formattedSeconds}`
+    workTittle.classList.add('active');
 }
 
-// calculate new times, update the page, handle ending timer
-function decrement() {
-  // don't do anything if paused
-  if (isPaused) return
+// start timer
+function start() {
+    // change button
+    document.getElementById('start').style.display = "none";
+    document.getElementById('reset').style.display = "block";
 
-  // update the count
-  currentSeconds--
+    // change the time
+    seconds = 59;
 
-  // format the count and update the page
-  updateTimer(currentTimer, currentSeconds)
+    let workMinutes = workTime - 1;
+    let breakMinutes = breakTime - 1;
 
-  // update progress based on new count
-  const percentage = (currentTotal - currentSeconds) / currentTotal
-  // update the progress bar
-  setProgress(percentage)
+    breakCount = 0;
 
-  // if timer ends
-  if (currentSeconds === 0) {
-    // stop the interval
-    clearInterval(timerInterval)
+    // countdown
+    let timerFunction = () => {
+        //change the display
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
 
-    if (isWorkPhase) {
-      isWorkPhase = false
-      // switch timers *phases
-      currentSeconds = breakSeconds
-      // currentTotal = breakTotal
-      currentTimer = breakTimer
-      workTimer.classList.remove("timer--active")
-      breakTimer.classList.add("timer--active")
-      // change the colours
-      document.body.classList.add("break-phase")
-      // start the new timer
-      timerInterval = setInterval(decrement, 1000)
-    } else {
-      // disable the buttons
-      controlBtn.classList.remove("control-btn-pause")
-      controlBtn.setAttribute("disabled", "disabled")
-      addTimeBtn.setAttribute("disabled", "disabled")
+        // start
+        seconds = seconds - 1;
+
+        if(seconds === 0) {
+            workMinutes = workMinutes - 1;
+            if(workMinutes === -1 ){
+                if(breakCount % 2 === 0) {
+                    // start break
+                    workMinutes = breakMinutes;
+                    breakCount++
+
+                    // change the painel
+                    workTittle.classList.remove('active');
+                    breakTittle.classList.add('active');
+                }else {
+                    // continue work
+                    workMinutes = workTime;
+                    breakCount++
+
+                    // change the painel
+                    breakTittle.classList.remove('active');
+                    workTittle.classList.add('active');
+                }
+            }
+            seconds = 59;
+        }
     }
-  }
-}
 
-function setProgress(newPercentageValue) {
-  // and fills in the circle border
-  const offset = circumference - (newPercentageValue * circumference)
-  circle.style.strokeDashoffset = offset
-
-  // sets the arm to rotate
-  const rotation = newPercentageValue * 360
-  arm.style.transform = `rotate(${rotation}deg)`
+    // start countdown
+    setInterval(timerFunction, 1000); // 1000 = 1s
 }
